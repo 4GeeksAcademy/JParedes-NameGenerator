bec859d82b0fa54acee0dc54920182eca25542a7
JParedesArtica <126907318+JParedesArtica@users.noreply.github.com>
2023-07-11 02:33:10 +0000

    NameGenerator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateNameRandom();
+
+  function generateNameRandom() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let aux = "";
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          aux += pronoun[i] + adj[j] + noun[k] + ".com" + "<br />";
+        }
+      }
+    }
+    return aux;
+  }
 };
